6e3dc58b56f36584ddeddc9e0a32d4b45fee739b
tkrzy <t.krzyzowski96@gmail.com>
2017-04-03 17:32:33 +0200

    BetterShit

--- app/src/test/java/krzyzowski/tomasz/lab2/lab2/CountBmiImperialTest.java
+++ app/src/test/java/krzyzowski/tomasz/lab2/lab2/CountBmiImperialTest.java
@@ -2,6 +2,7 @@ package krzyzowski.tomasz.lab2.lab2;
 
 import org.junit.Test;
 
+import krzyzowski.tomasz.lab2.lab2.CounterBMI.CountBMI;
 import krzyzowski.tomasz.lab2.lab2.CounterBMI.CountBmiImperial;
 
 import static org.junit.Assert.assertEquals;
@@ -12,35 +13,35 @@ import static org.junit.Assert.assertTrue;
  * Created by tkrzy on 27.03.2017.
  */
 
-public class CountLbFtBMITest extends PrepareCounterBMI {
-    public CountLbFtBMITest() {
-        super(new CountBmiImperial());
-    }
+public class CountBmiImperialTest {
+
+    private CountBMI counterBMI = new CountBmiImperial();
+
     @Test
     public void weight_positive_number_is_invalid() {
         float testWeight = 20.0f; //9kg
 
         assertFalse(counterBMI.isWeightValid(testWeight));
     }
+
     @Test
     public void weight_under_zero_is_invalid() {
-        //GIVEN
         float testWeight = -1.0f;
-        //WHEN
-        //THEN
         boolean actual = counterBMI.isWeightValid(testWeight);
 
         assertFalse(actual);
     }
+
     @Test
     public void is_valid_bmi() {
         float testWeight = 149.91f; //68kg
         float testHeight = 5.57f;  //1.7m
         float actualBMI = 23.58f; // actualBmi
 
-        assertEquals(actualBMI, counterBMI.countBmi(testWeight,testHeight),0.01);
+        assertEquals(actualBMI, counterBMI.countBmi(testWeight, testHeight), 0.01);
     }
-    @Test (expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void isThrowingIllegalArgumentException() {
 
         float testWeight = -68f;
@@ -48,19 +49,22 @@ public class CountLbFtBMITest extends PrepareCounterBMI {
 
         float actualBMI = counterBMI.countBmi(testWeight, testHeight);
     }
+
     @Test
     public void height_under_zero_is_invalid() {
-        float testHeight =  -1.7f;
+        float testHeight = -1.7f;
         boolean actual = counterBMI.isHeightValid(testHeight);
 
         assertFalse(actual);
     }
+
     @Test
     public void height_is_valid() {
         float testHeight = 5.90f; //1.8m
 
         assertTrue(counterBMI.isHeightValid(testHeight));
     }
+
     @Test
     public void height_positive_number_is_invalid_() {
         float testHeight = 0.98f;//0.3m
--- app/src/test/java/krzyzowski/tomasz/lab2/lab2/CountBmiMetricalTest.java
+++ app/src/test/java/krzyzowski/tomasz/lab2/lab2/CountBmiMetricalTest.java
@@ -2,6 +2,7 @@ package krzyzowski.tomasz.lab2.lab2;
 
 import org.junit.Test;
 
+import krzyzowski.tomasz.lab2.lab2.CounterBMI.CountBMI;
 import krzyzowski.tomasz.lab2.lab2.CounterBMI.CountBmiMetrical;
 
 import static org.junit.Assert.*;
@@ -10,69 +11,60 @@ import static org.junit.Assert.*;
  * Created by tkrzy on 15.03.2017.
  */
 
-public class CountKgMBmiTest extends PrepareCounterBMI {
+public class CountBmiMetricalTest {
 
-    public CountKgMBmiTest(){
-        super(new CountBmiMetrical());
-    }
+    private CountBMI counterBMI = new CountBmiMetrical();
+
     @Test
     public void weight_positive_number_is_invalid() {
         float testWeight = 9.0f;
 
         assertFalse(counterBMI.isWeightValid(testWeight));
     }
+
     @Test
     public void weight_under_zero_is_invalid() {
-      //GIVEN
         float testWeight = -1.0f;
-      //WHEN
-      //THEN
         boolean actual = counterBMI.isWeightValid(testWeight);
 
         assertFalse(actual);
     }
+
     @Test
     public void is_valid_bmi() {
         float testWeight = 68;
         float testHeight = 1.7f;
         float actualBMI = 23.53f;
 
-        assertEquals(actualBMI, counterBMI.countBmi(testWeight,testHeight),0.01);
+        assertEquals(actualBMI, counterBMI.countBmi(testWeight, testHeight), 0.01);
     }
-    @Test (expected = IllegalArgumentException.class)
-    public void isThrowingIllegalArgumentException() {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void isThrowingIllegalArgumentException() {
         float testWeight = -68f;
         float testHeight = 1.7f;
 
         float actualBMI = counterBMI.countBmi(testWeight, testHeight);
     }
+
     @Test
     public void height_under_zero_is_invalid() {
-        float testHeight =  -1.7f;
+        float testHeight = -1.7f;
         boolean actual = counterBMI.isHeightValid(testHeight);
 
         assertFalse(actual);
     }
+
     @Test
     public void height_is_valid() {
         float testHeight = 1.8f;
 
         assertTrue(counterBMI.isHeightValid(testHeight));
     }
+
     @Test
     public void height_positive_number_is_invalid_() {
         float testHeight = 0.3f;
         assertFalse(counterBMI.isHeightValid(testHeight));
     }
-
-    //to do
-
-    /*
-        MASS filtrowac Text view i edit Text
-        Height to samo
-        (Btn) Count
-        BMI :
-         20
-     */
 }
