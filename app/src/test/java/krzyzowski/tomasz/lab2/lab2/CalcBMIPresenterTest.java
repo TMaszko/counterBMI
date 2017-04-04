86933df41bd931ba53272fa346bfda73c865bf58
tkrzy <t.krzyzowski96@gmail.com>
2017-04-04 21:38:05 +0200

    ShitLOL v3

--- app/src/test/java/krzyzowski/tomasz/lab2/lab2/CalcBMIPresenterTest.java
+++ app/src/test/java/krzyzowski/tomasz/lab2/lab2/CalcBMIPresenterTest.java
@@ -3,7 +3,6 @@ package krzyzowski.tomasz.lab2.lab2;
 import org.junit.Before;
 import org.junit.Test;
 
-import krzyzowski.tomasz.lab2.lab2.CounterBMI.CountBmiImperial;
 import krzyzowski.tomasz.lab2.lab2.CounterBMI.CountBmiMetrical;
 
 import static org.mockito.Mockito.*;
@@ -35,55 +34,55 @@ public class CalcBMIPresenterTest {
 
     @Test
     public void emptyWeightDisablesCountButton() {
-        presenter.onInputEdit("", VALID_HEIGHT_TEXT);
+        presenter.onInputChanged("", VALID_HEIGHT_TEXT);
         verify(view).disableCountBtn();
     }
 
     @Test
     public void emptyHeightDisablesCountButton() {
-        presenter.onInputEdit(VALID_WEIGHT_TEXT, "");
+        presenter.onInputChanged(VALID_WEIGHT_TEXT, "");
         verify(view).disableCountBtn();
     }
 
     @Test
     public void validInputsEnableCountButton() {
-        presenter.onInputEdit(VALID_WEIGHT_TEXT, VALID_HEIGHT_TEXT);
+        presenter.onInputChanged(VALID_WEIGHT_TEXT, VALID_HEIGHT_TEXT);
         verify(view).enableCountBtn();
     }
 
     @Test
     public void invalidWeightAndValidHeightShowsErrorMsg() {
-        presenter.onInputEdit(INVALID_WEIGHT_TEXT, VALID_HEIGHT_TEXT);
+        presenter.onInputChanged(INVALID_WEIGHT_TEXT, VALID_HEIGHT_TEXT);
         verify(view).showWeightError(Float.valueOf(INVALID_WEIGHT_TEXT), CountBmiMetrical.MIN_WEIGHT, CountBmiMetrical.MAX_WEIGHT);
     }
 
     @Test
     public void invalidHeightAndValidWeightShowsErrorMsg() {
-        presenter.onInputEdit(VALID_WEIGHT_TEXT, INVALID_HEIGHT_TEXT);
+        presenter.onInputChanged(VALID_WEIGHT_TEXT, INVALID_HEIGHT_TEXT);
         verify(view).showHeightError(Float.valueOf(INVALID_HEIGHT_TEXT), CountBmiMetrical.MIN_HEIGHT, CountBmiMetrical.MAX_HEIGHT);
     }
 
     @Test
     public void invalidWeightAndValidHeightDisablesCountButton() {
-        presenter.onInputEdit(INVALID_WEIGHT_TEXT, VALID_HEIGHT_TEXT);
+        presenter.onInputChanged(INVALID_WEIGHT_TEXT, VALID_HEIGHT_TEXT);
         verify(view).disableCountBtn();
     }
 
     @Test
     public void invalidHeightDisablesCountButton() {
-        presenter.onInputEdit(VALID_WEIGHT_TEXT, INVALID_HEIGHT_TEXT);
+        presenter.onInputChanged(VALID_WEIGHT_TEXT, INVALID_HEIGHT_TEXT);
         verify(view).disableCountBtn();
     }
 
     @Test
     public void invalidWeightEmptyHeightShowErrorMsg() {
-        presenter.onInputEdit(INVALID_WEIGHT_TEXT, "");
+        presenter.onInputChanged(INVALID_WEIGHT_TEXT, "");
         verify(view).showWeightError(Float.valueOf(INVALID_WEIGHT_TEXT),CountBmiMetrical.MIN_WEIGHT, CountBmiMetrical.MAX_WEIGHT);
     }
 
     @Test
     public void invalidHeightEmptyWeightShowErrorMsg() {
-        presenter.onInputEdit("",INVALID_HEIGHT_TEXT);
+        presenter.onInputChanged("",INVALID_HEIGHT_TEXT);
         verify(view).showHeightError(Float.valueOf(INVALID_HEIGHT_TEXT),CountBmiMetrical.MIN_HEIGHT, CountBmiMetrical.MAX_HEIGHT);
     }
 
